3bcb32c35d22a11f67c0337f4639d05ac3cebb1e
shortlikeafox <62516729+shortlikeafox@users.noreply.github.com>
2020-03-26 10:29:10 -0500

    Created split_event function
    1. Created split_event function
    2. in create_fight_df function we now convert the 'date' feature to DateTime type.

--- test.py
+++ test.py
@@ -20,14 +20,26 @@ pd.set_option('display.max_rows', 500) #Used for debugging
 
 #print(df.head)
 
-#print(df.dtypes)
 
 #OK... Now we need to create some dataframes....
 
 df = h.create_master_df()
+print(df.dtypes)
 
-print(df.head)
+
+#print(df.head)
 print(len(df))
 
+date_df = df['date']
+print(date_df.dtypes)
+
+
+master_df, event_df = h.split_event("02-29-2020", df)
+
+print(len(master_df))
+print(len(event_df))
+
+print(event_df['Winner'])
+print(event_df['label'])
 
 df.to_csv('testfile.csv')
